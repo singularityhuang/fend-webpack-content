45ec08f10c74d0903f00f1a8948641db6bdce0af
Petula <petula@Petulas-MacBook-Pro.local>
2022-09-28 20:56:07 +0800

    update

--- src/server/index.js
+++ src/server/index.js
@@ -15,6 +15,7 @@ app.get('/', function (_req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
+    console.log('http://localhost:8080')
 })
 
 app.get('/test', function (_req, res) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -13,4 +14,5 @@ module.exports = {
             }
         ]
     }
+
 }
